2409beea7b1522f8c3cd7f777fa2e78db37e1db4
Botagoz Amangeldiyeva <botagoz.amangeldiyeva@gmail.com>
2025-10-29 21:48:57 +0500

    few more improvements

--- src/ConfirmedBooking.js
+++ src/ConfirmedBooking.js
@@ -1,10 +1,22 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
 const ConfirmedBooking = () => {
-    return (
-        <div className="ConfirmedBooking">
-            <h2>Thank you for your reservation!</h2>
-            <p>We look forward to serving you.</p>
-        </div>
-    );
-}
+  const navigate = useNavigate();
+
+  const handleReturnHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="ConfirmedBooking">
+      <h2>Thank you for your reservation!</h2>
+      <p>We look forward to serving you</p><br/>
+      <button onClick={handleReturnHome} className="return-button">
+        Return to Homepage
+      </button>
+    </div>
+  );
+};
 
 export default ConfirmedBooking;
